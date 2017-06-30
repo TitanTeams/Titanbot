42975f30b1f0d07be4a391e6ba6139a3f67a05ca
Titan teams <pvresanm@gmail.com>
2017-06-30 05:04:59 +0430

    v.1

--- auto.sh
+++ auto.sh
@@ -5,3 +5,4 @@ while(true) do
 curl "https://api.telegram.org/bot[ToKeN]/sendmessage" -F "chat_id=308444837" -F "text=#NEWCRASH-Reloaded-${COUNTER}-times"
 let COUNTER=COUNTER+1 
 done
+-- @titanteams
